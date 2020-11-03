95f6441d809e098ff8dbfed720bd94f5e2f07a00
ellen <ellen.dalecio@gmail.com>
2020-11-03 00:03:57 -0300

    Delete seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace SalesWebMvc.Controllers
 {
@@ -38,5 +39,29 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
